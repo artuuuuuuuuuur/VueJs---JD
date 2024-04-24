1a6b20b2519a5caa49af6215104fdbaafd135af1
artur <arturdemaria7@gmail.com>
2024-04-23 21:00:51 -0300

    aula 1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Primeira aula de Vue... {{ nome }}</h1>
+        <input type="text" v-model="nome" />
+        <Button text="Teste"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from "vue";
+    import Button from "./components/Button.vue";
+
+    const nome = ref<String>("");
+</script>
+
+<style scoped></style>
---
+++ src/components/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+<script setup lang="ts">
+
+  const props = defineProps<{
+    text: string;
+  }>();
+</script>
+<style scoped>
+</style>
